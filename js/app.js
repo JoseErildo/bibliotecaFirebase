72c4ee649166ae2dec472242edbcf9c44ca29c56
JoseErildo <joseerildo007@gmail.com>
2020-11-26 10:55:38 -0300

    att

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDfLAbdeNyf9L2VY1f46HdxVw08Z5Tgaww",
+    authDomain: "biblioteca-b55ba.firebaseapp.com",
+    databaseURL: "https://biblioteca-b55ba.firebaseio.com",
+    projectId: "biblioteca-b55ba",
+    storageBucket: "biblioteca-b55ba.appspot.com",
+    messagingSenderId: "608044131492",
+    appId: "1:608044131492:web:c612f5f6168237d0593231",
+    measurementId: "G-C0VZW2JD3Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
